aef6cd6664ad251fd13b3fde56e0ac2ae67e0261
JEONGSUJI <wtw9611@gmail.com>
2019-12-18 15:54:11 +0900

    cbv 사용

--- polls/urls.py
+++ polls/urls.py
@@ -1,6 +1,6 @@
 from django.urls import path
 
-from .views import index, detail, vote, results
+from .views import index, detail, vote, results, IndexView, DetailView, ResultsView
 
 app_name = 'polls'
 
@@ -9,4 +9,8 @@ urlpatterns = [
     path('<int:question_id>/', detail, name='detail'),
     path('<int:question_id>/results/', results, name='results'),
     path('<int:question_id>/vote/', vote, name='vote'),
+
+    path('cbv/', IndexView.as_view(), name='cbv-index'),
+    path('cbv/<int:pk>/', DetailView.as_view(), name='cbv-detail'),
+    path('cbv/<int:pk>/', ResultsView.as_view(), name='cbv-results'),
 ]
